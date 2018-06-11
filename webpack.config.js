7566562f27dc99175e77a220b64146f4b415c1dc
mjcampagna <mjcampagna@gmail.com>
2018-06-11 14:42:35 -0700

    Initial setup and config of repo

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './client/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js'
+	},
+	watch: true,
+  
+  module: {
+    rules: [
+
+      { 
+        test: /\.jsx?$/, 
+        loader: 'babel-loader',
+        include: /client/,
+        options: {
+          presets: ['env', 'react']
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+
+    ]//rules
+  },//module
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      title: 'Itsy',
+      template: 'client/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "app.css"
+    })
+
+
+  ]//plugins
+
+};
