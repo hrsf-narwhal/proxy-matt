30cc71e4e664826dcaa22b8291d2c5075b143d41
mjcampagna <mjcampagna@gmail.com>
2018-06-11 19:40:05 -0700

    Successfully getting images from the component server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const path = require('path');
+const axios = require('axios');
 const express = require('express')
 const app = express();
 
 const publicPath = path.resolve(__dirname, '../public');
-app.use(express.static( publicPath ));
+app.use('/listing/:pid', express.static( publicPath ));
 
-// respond with "hello world" when a GET request is made to the homepage
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
+app.get('/images/:pid', (req, res) => {
+
+	axios.get( 'http://localhost:3001' + req.url, {
+
+	})
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening on port ${port}...`));
