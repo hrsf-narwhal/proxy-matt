affee02cec38fd697c63042adeac13845613c9b4
mjcampagna <mjcampagna@gmail.com>
2018-06-13 11:20:41 -0700

    Add all components to proxy, and some layout

--- server/index.js
+++ server/index.js
@@ -8,14 +8,60 @@ app.use('/listing/:pid', express.static( publicPath ));
 
 app.get('/images/:pid', (req, res) => {
 
-	axios.get( 'http://localhost:3001' + req.url, {
-
-	})
+	axios.get( 'http://localhost:3001' + req.url )
 	.then(response => {
 		res.status(200).send(response.data);
 	})
 	.catch(err => console.log(err));
 
+});
+
+app.get('/api/listing/:productID', (req, res) => {
+	axios.get( 'http://localhost:3002' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+});
+
+app.post('/api/listing/:productID', (req, res) => {
+	axios.post( 'http://localhost:3002' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+});
+
+app.get('/purchase/cart/:pid', (req, res) => {
+	axios.get( 'http://localhost:3003' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+});
+
+app.get('/purchase/item/:pid', (req, res) => {
+	axios.get( 'http://localhost:3003' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+});
+
+app.get('/purchase/shippingInfo/:pid', (req, res) => {
+	axios.get( 'http://localhost:3003' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
+});
+
+app.get('/purchase/shippingCost/:departure/:destination', (req, res) => {
+	axios.get( 'http://localhost:3003' + req.url )
+	.then(response => {
+		res.status(200).send(response.data);
+	})
+	.catch(err => console.log(err));
 });
 
 const port = process.env.PORT || 3000;
